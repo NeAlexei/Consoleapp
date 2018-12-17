e4ddb109265ad0f9de79455dee872b8833720b62
NAlexei <nechytaila89@gmail.com>
2018-12-18 00:28:37 +0300

    commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,37 +1,36 @@
 package com.company;
-import console.Console;
-import file.File;
 
 import java.io.*;
-import java.io.FileNotFoundException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException, NoSuchElementException {
 
-        Scanner scanner = new Scanner(System.in); //system.in отвечает за ввод в консоль.
+        Scanner scn = new Scanner(System.in); //system.in отвечает за считывание из консоли.
+        System.out.println("MAKE YOUR CHOICE" + "\n" + "1 - read from file" + "\n" + "2 - read from console");
 
-        System.out.println("Please, enter 1 or 2 " + "\n" + "1 - read from file" + "\n" + "2 - read from console");
-
-        int number = scanner.nextInt();
-        while (number != 1 & number != 2) {   // если условие цикла истинно, то выполняется блок кода за условием. Если ложно, то цикл пропускается.
-            System.out.println(number + " is not 1 or 2." + "\n" + "Please, enter only 1 or 2" + ": ");
-            number = scanner.nextInt();
+        int number = scn.nextInt();
+        while (number !=1 & number !=2){ //
+            System.out.println("Please, enter 1 or 2");
+            number = scn.nextInt();
         }
-        System.out.println("Your number is: " + number);
 
-        if (number == 1) {
-            System.out.println("Read from file");
-        } else {
-            System.out.println("Read from console");
+        if(number==1){
+            System.out.println("---Read from file---");
+
+            String path = "E:/Test.txt"; //в переменную записываем путь до файла.
+            File file = new File(path); //в скобках указываем путь до необходимого файла.
+            Scanner scanner = new Scanner(file); //можно считать все строки из этого файла.
+            System.out.println(scanner.nextLine());
+        }
+        else{
+            System.out.println("---Read from console---" + "\n" + "Enter your text: ");
+
+            Scanner sc = new Scanner(System.in);
+            String s = sc.next();
+            System.out.println("Your text: " + " " + s);
         }
     }
 }
-
-/*
-        String path = "D:/Java/test.txt"; //записываем в переменную путь до файла.
-        File file = new File(path); //в скобках указываем путь до необходимого файла.
-        Scanner scn = new Scanner(file); //можно считать все строки из этого файла.
-        }
-*/
